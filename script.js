6b3814c25c14fe725e67c31625d0d561f06ff6b4
Cristian Rios <criseduardo0503@gmail.com>
2022-10-29 19:12:23 -0500

    aaaaa

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ for (var i=0; i< names.length; i++ ) {
      helloSpeaker.speak(names[i]);
   }
 }
+
+(function(window) {
+
+ var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+}
+
+
+ window.helloSpeaker = helloSpeaker;
+})(window);
+
+(function(window){
+
+ var byeSpeaker = {};
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+})(window)
